0185df84a37e636f8a14850998d20db8121063af
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-10 20:04:05 +0900

    feat 기본적인 프로젝트 구성 완료

---
+++ src/main/java/com/openvelog/openvelogbe/OpenVelogBeApplication.java
@@ -0,0 +1,13 @@
+package com.openvelog.openvelogbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenVelogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenVelogBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openvelog/openvelogbe/OpenVelogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openvelog.openvelogbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenVelogBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
