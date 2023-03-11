500ccf03c954f0162c29f94ca103e3b00692638b
kikidy12 <kikidy12@naver.com>
2023-03-12 08:49:53 +0900

    feat : userDetails 설정 #23
    ErrorMessage수정
    UserDetailsServiceImpl, UserDetailsImpl 추가

---
+++ src/main/java/com/openvelog/openvelogbe/common/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.openvelog.openvelogbe.common.security;
+
+import com.openvelog.openvelogbe.common.entity.Member;
+import com.openvelog.openvelogbe.common.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member user = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return new UserDetailsImpl(user, user.getUsername());
+    }
+
+}
