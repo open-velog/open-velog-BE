cf0c317814039d1392be5234ce2e4d3204c669ac
kikidy12 <kikidy12@naver.com>
2023-03-13 20:00:25 +0900

    refactor : 게시판 등록, 삭제, 수정, 단일조회 수정 #48
    member중복호출 제거, 코드 정리, 게시글 등록, 수정 dto분리, 업데이트시 blogId body로 옮김

--- src/main/java/com/openvelog/openvelogbe/board/controller/BoardController.java
+++ src/main/java/com/openvelog/openvelogbe/board/controller/BoardController.java
@@ -23,29 +23,28 @@ import javax.validation.Valid;
 public class BoardController {
     private final BoardService boardService;
 
-    @PostMapping()
+    @PostMapping
     @Operation(summary = "게시글 작성", description ="해당 블로그Id를 갖는 블로그에 게시글 작성")
-    public ApiResponse<BoardResponseDto> createBoard
-            (@RequestParam(value = "blogId") Long blogId,
-             @RequestBody @Valid BoardRequestDto requestDto,
+    public ApiResponse<BoardResponseDto> createBoard (
+             @RequestBody @Valid BoardRequestDto.BoardAdd dto,
              @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return ApiResponse.successOf(HttpStatus.CREATED, boardService.createBoard(blogId,requestDto,userDetails));
+        return ApiResponse.successOf(HttpStatus.CREATED, boardService.createBoard(dto, userDetails));
     }
 
     @GetMapping("/{boardId}")
     @SecurityRequirements()
     @Operation(summary = "게시글 조회", description ="특정 boardId를 갖는 단일 게시글 조회")
     public ApiResponse<BoardResponseDto> getBoard(@PathVariable Long boardId){
-        return ApiResponse.successOf(HttpStatus.OK,boardService.getBoard(boardId));
+        return ApiResponse.successOf(HttpStatus.OK, boardService.getBoard(boardId));
     }
 
     @PutMapping("/{boardId}")
     @Operation(summary = "게시글 수정", description ="특정 boardId의 게시글 수정")
     public ApiResponse<BoardResponseDto> updateBoard
             (@PathVariable Long boardId,
-             @RequestBody @Valid BoardRequestDto requestDto,
+                    @RequestBody @Valid BoardRequestDto.BoardUpdate dto,
              @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return ApiResponse.successOf(HttpStatus.CREATED,boardService.updateBoard(boardId,requestDto,userDetails));
+        return ApiResponse.successOf(HttpStatus.CREATED, boardService.updateBoard(boardId, dto, userDetails));
     }
 
     @DeleteMapping("/{boardId}")
@@ -54,6 +53,6 @@ public class BoardController {
             @PathVariable Long boardId,
             @Parameter(hidden = true) @AuthenticationPrincipal UserDetailsImpl userDetails) {
         boardService.deleteBoard(boardId,userDetails);
-        return ApiResponse.successOf(HttpStatus.NO_CONTENT,null);
+        return ApiResponse.successOf(HttpStatus.NO_CONTENT, null);
     }
 }
--- src/main/java/com/openvelog/openvelogbe/board/dto/BoardRequestDto.java
+++ src/main/java/com/openvelog/openvelogbe/board/dto/BoardRequestDto.java
@@ -5,15 +5,35 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Setter
 @Getter
 @AllArgsConstructor
 public class BoardRequestDto {
-    @NotBlank
-    @Size(min=3, max=30, message = "제목은 3자 이상 30자 이하이여야합니다.")
-    private String title;
-    @NotBlank
-    private String content;
+
+    @Getter
+    @AllArgsConstructor
+    public static class BoardAdd {
+
+        @NotNull
+        private Long blogId;
+        @NotBlank
+        @Size(min=3, max=30, message = "제목은 3자 이상 30자 이하이여야합니다.")
+        private String title;
+        @NotBlank
+        private String content;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class BoardUpdate {
+
+        @NotBlank
+        @Size(min=3, max=30, message = "제목은 3자 이상 30자 이하이여야합니다.")
+        private String title;
+        @NotBlank
+        private String content;
+    }
 }
