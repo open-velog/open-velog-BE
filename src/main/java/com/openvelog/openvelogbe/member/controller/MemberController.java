fb30d35d08193c5a12d516c8f82be5912333151e
vegeterianlee <max322318@gmail.com>
2023-03-13 16:00:32 +0900

    fix: MemberService 수정 #36 - 로그인 에러 메세지 수정 - SignupRequestDto @Valid 오류해결 - @EnableJpaAuditing 추가

--- src/main/java/com/openvelog/openvelogbe/member/controller/MemberController.java
+++ src/main/java/com/openvelog/openvelogbe/member/controller/MemberController.java
@@ -6,6 +6,8 @@ import com.openvelog.openvelogbe.member.dto.MemberResponseDto;
 import com.openvelog.openvelogbe.member.dto.SignupRequestDto;
 import com.openvelog.openvelogbe.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,16 +24,18 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
+    @SecurityRequirements()
     @Operation(summary = "회원 가입", description = "userId은 영문숫자 조합 4자 이상, 10자 이하\n password은 영문숫자 조합 8자 이상, 15자 이하\n, username은 아무 문자 4자 이상 20자 이하")
     public ApiResponse<MemberResponseDto> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         return ApiResponse.successOf(HttpStatus.CREATED, memberService.signup(signupRequestDto));
     }
 
     @PostMapping("/login")
+    @SecurityRequirements()
     @Operation(summary = "로그인")
     public ApiResponse<MemberResponseDto> login(
             @RequestBody LoginRequestDto loginRequestDto,
-            HttpServletResponse response
+            @Parameter(hidden = true) HttpServletResponse response
     ) {
         return ApiResponse.successOf(HttpStatus.CREATED, memberService.login(loginRequestDto, response));
     }
