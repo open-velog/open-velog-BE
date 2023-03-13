556798f553a78178fdcb2f5acb819512b1e2755c
kikidy12 <kikidy12@naver.com>
2023-03-13 17:22:46 +0900

    feat : 블로그 등록 수정 삭제 단일조회 #32
    blog등록, 수정, 삭제, 단일 조회 api추가, NO_BLOG에러메시지 추가, .antMatchers(HttpMethod.GET, "/api/blogs/{blogId}").permitAll()추가

--- src/main/java/com/openvelog/openvelogbe/common/dto/ErrorMessage.java
+++ src/main/java/com/openvelog/openvelogbe/common/dto/ErrorMessage.java
@@ -17,7 +17,8 @@ public enum ErrorMessage {
     USERID_DUPLICATION("userid가 중복됐습니다."),
     WRONG_USERNAME("username이 일치하지 않습니다."),
     WRONG_PASSWORD("패스워드가 틀렸습니다."),
-    WRONG_JWT_TOKEN("JWT Token이 잘못되었습니다.");
+    WRONG_JWT_TOKEN("JWT Token이 잘못되었습니다."),
+    NO_BLOG("블로그가 없습니다.");
 
     private final String message;
 
