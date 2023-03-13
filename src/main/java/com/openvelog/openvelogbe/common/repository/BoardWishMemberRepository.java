46a636d795f04e1b542c4ad3229e584370fcdd55
kikidy12 <kikidy12@naver.com>
2023-03-13 21:30:41 +0900

    feat : 게시글 좋아요 관리 #55
    게시글 좋아요 등록/취소 api추가, 게시글 삭제시 wish도 삭제처리, member삭제시에는 setnull처리

--- src/main/java/com/openvelog/openvelogbe/common/repository/BoardWishMemberRepository.java
+++ src/main/java/com/openvelog/openvelogbe/common/repository/BoardWishMemberRepository.java
@@ -1,7 +1,14 @@
 package com.openvelog.openvelogbe.common.repository;
 
+import com.openvelog.openvelogbe.common.entity.Board;
 import com.openvelog.openvelogbe.common.entity.BoardWishMember;
+import com.openvelog.openvelogbe.common.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface BoardWishMemberRepository extends JpaRepository<BoardWishMember, Long> {
+    Optional<BoardWishMember> findByMemberAndBoard(Member member, Board board);
+
 }
