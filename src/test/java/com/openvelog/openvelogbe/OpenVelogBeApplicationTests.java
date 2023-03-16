b7c82d4e723d1d9168c64903a425c00a27802766
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-16 20:20:44 +0900

    chore: test를 위한 application.properties를 따로 만들어줬음 #100

--- src/test/java/com/openvelog/openvelogbe/OpenVelogBeApplicationTests.java
+++ src/test/java/com/openvelog/openvelogbe/OpenVelogBeApplicationTests.java
@@ -2,10 +2,14 @@ package com.openvelog.openvelogbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @EnableJpaAuditing
+@EnableCaching
+@ActiveProfiles("test")
 class OpenVelogBeApplicationTests {
 
     @Test
