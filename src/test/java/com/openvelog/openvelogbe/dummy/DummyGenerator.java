7388ae21af7bc92b2e2a2e1a9a41d656f9f624f0
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-16 13:09:16 +0900

    refactor: Dummy Data를 만들어주는 interface를 abstract로 리팩토링 #100

--- src/test/java/com/openvelog/openvelogbe/dummy/DummyGenerator.java
+++ src/test/java/com/openvelog/openvelogbe/dummy/DummyGenerator.java
@@ -1,9 +1,49 @@
 package com.openvelog.openvelogbe.dummy;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
+import java.util.Random;
 
-public interface DummyGenerator<T> {
-    T generateDummyOfThis();
+@Slf4j
+public abstract class DummyGenerator<E, R extends JpaRepository> {
+    protected Random random = new Random();
 
-    List<T> generateDummiesOfThis(long dummyCount);
+    protected final R repository;
+
+    DummyGenerator(R repository) {
+        this.repository = repository;
+    }
+
+    protected abstract E generateDummyEntityOfThis();
+
+    protected boolean insertDummyIntoDatabase() {
+        E generatedDummyEntity = generateDummyEntityOfThis();
+        boolean isSucceeded = true;
+
+        try {
+            repository.save(generatedDummyEntity);
+        } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+            log.error("Unique property got duplicated.");
+            isSucceeded = false;
+        } catch (RuntimeException runtimeException) {
+            runtimeException.printStackTrace();
+            isSucceeded = false;
+        }
+
+        return isSucceeded;
+    }
+
+    protected List<E> insertDummiesIntoDatabase(long dummyCount) {
+        long dummyInserted = 0;
+        while (dummyInserted < dummyCount) {
+            dummyInserted += insertDummyIntoDatabase() ? 1 : 0;
+        }
+
+        return  repository.findAll();
+    }
 }
