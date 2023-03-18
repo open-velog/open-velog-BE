76774b6db5dc2c71663acb8325761e29623f1abd
kikidy12 <kikidy12@naver.com>
2023-03-18 21:11:43 +0900

    feat : 키워드 검색 수정

--- src/main/java/com/openvelog/openvelogbe/common/repository/BoardRepository.java
+++ src/main/java/com/openvelog/openvelogbe/common/repository/BoardRepository.java
@@ -19,7 +19,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 //    @EntityGraph(attributePaths = {
 //            "wishes", "blog", "blog.member"
 //    })
-    @Query(value = "SELECT * FROM boards WHERE MATCH(title, content) AGAINST(:keyword IN BOOLEAN MODE)",nativeQuery = true)
+    @Query(value = "select * from boards where match(title) against(:keyword) or match(content) against(:keyword)",nativeQuery = true)
     Page<Board> searchTitleOrContentOrBlogTitle(@Param("keyword") String keyword, Pageable pageable);
     Optional<Board> findById(Long boardId);
 
