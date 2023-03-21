afe53568e181db110c8fdea233f9b15daf6bd555
kikidy12 <kikidy12@naver.com>
2023-03-21 22:20:40 +0900

    refactor: 검색 쿼리 성능 개선 #120
    현재 페이지 표시

--- src/main/java/com/openvelog/openvelogbe/board/dto/BoardResponseAndCountDto.java
+++ src/main/java/com/openvelog/openvelogbe/board/dto/BoardResponseAndCountDto.java
@@ -18,6 +18,8 @@ public class BoardResponseAndCountDto {
 
     Integer totalPages = 0;
 
+    Integer pageNumber = 0;
+
     Integer totalElements = 0;
 
     Integer numberOfElements = 0;
@@ -27,7 +29,7 @@ public class BoardResponseAndCountDto {
 
     public static BoardResponseAndCountDto of(List<BoardResponseDto> content, Integer currentPage, Integer perPage, Integer totalCount) {
         BoardResponseAndCountDtoBuilder builder = builder()
-                .content(content).totalElements(totalCount).numberOfElements(totalCount);
+                .content(content).totalElements(totalCount).pageNumber(currentPage).numberOfElements(totalCount);
 
         if (totalCount == 0) {
             return builder.totalPages(0).last(true).build();
@@ -35,7 +37,7 @@ public class BoardResponseAndCountDto {
 
         Integer page = (totalCount/perPage) + 1;
 
-        builder.totalPages(page).last(page.equals(currentPage));
+        builder.totalPages(page).last(page.equals(currentPage - 1));
 
 
         return builder.build();
