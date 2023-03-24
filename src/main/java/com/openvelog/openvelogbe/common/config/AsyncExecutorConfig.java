f0194d78ad23d6a8f021c67a815c5434f5987604
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2023-03-24 13:38:07 +0900

    feat: Enhance search performance by applying asynchronous function #138

---
+++ src/main/java/com/openvelog/openvelogbe/common/config/AsyncExecutorConfig.java
@@ -0,0 +1,24 @@
+package com.openvelog.openvelogbe.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncExecutorConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(100);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Async Executor");
+        executor.initialize();
+        return executor;
+    }
+}
