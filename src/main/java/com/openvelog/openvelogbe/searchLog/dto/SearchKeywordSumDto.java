db952869f50117a9cf9c00cdab36e1a2369f4057
kikidy12 <kikidy12@naver.com>
2023-04-15 15:47:49 +0900

    feat : mongodb 설정 및 검색 로그 수집 및 집계 수정 #190
    mongodb 설정
    MongoSearchLog에 검색 로그 저장
    랭킹 조회 수정
    트랜드 집계처리 수정

--- src/main/java/com/openvelog/openvelogbe/searchLog/dto/SearchKeywordSumDto.java
+++ src/main/java/com/openvelog/openvelogbe/searchLog/dto/SearchKeywordSumDto.java
@@ -1,13 +1,22 @@
 package com.openvelog.openvelogbe.searchLog.dto;
 
+import com.openvelog.openvelogbe.common.entity.enums.AgeRange;
+import com.openvelog.openvelogbe.common.entity.enums.Gender;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchKeywordSumDto {
     private String keyword;
-    private int count;
-    private String gender;
-    private String ageRange;
+    private Gender gender;
+    private AgeRange ageRange;
+    private Long count;
 }
